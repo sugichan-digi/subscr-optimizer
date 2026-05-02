a423efbc00630ddac1351fac1048e7836af48d83
Sugichan <sugiyama.d@outlook.com>
2026-05-03 03:12:33 +0900

    feat: Google Analyticsの追加と本番環境設定の反映

--- frontend/common.js
+++ frontend/common.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // APIのベースURL
-const API_BASE = 'http://localhost:8000';
-// const API_BASE = 'https://api-subsc-manage.rbtm.jp/';
+// const API_BASE = 'http://localhost:8000';
+const API_BASE = 'https://api-subsc-manage.rbtm.jp/';
 
 
 /* ===== Shared API & Session ===== */
